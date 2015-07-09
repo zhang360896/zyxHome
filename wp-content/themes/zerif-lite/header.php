de97bbbd63003d7b3b0d9be740053aa108170ac7
360896270@qq.com <360896270@qq.com>
2015-07-09 17:51:04 +0800

    Change the music ID to 13

--- wp-content/themes/zerif-lite/header.php
+++ wp-content/themes/zerif-lite/header.php
@@ -79,7 +79,7 @@ endif; ?>
 
 
 <header id="home" class="header">
-<?php echo do_shortcode('[amazingaudioplayer id="5"]'); ?>
+<?php echo do_shortcode('[amazingaudioplayer id="13"]'); ?>
 	<div id="main-nav" class="navbar navbar-inverse bs-docs-nav" role="banner">
 
 		<div class="container">
