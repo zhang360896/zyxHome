1a9ad8287d4cd60d9c0fa9a1d425668a9e750628
yizhenkai <1378963360@qq.com>
2016-04-09 16:33:04 +0800

    无法显示

--- wp-content/themes/zerif-lite/template-living-room.php
+++ wp-content/themes/zerif-lite/template-living-room.php
@@ -11,7 +11,17 @@ get_header(); ?>
 <div class="clear"></div>
 
 </header> <!-- / END HOME SECTION  -->
-
+<div class="view">
+		<canvas id="cas" width="240" height="352""style="display:none;" >您的浏览器不支持canvas，请升级浏览器</canvas> 
+		<img src="http://7xjrp9.com1.z0.glb.clouddn.com/white_ball.png" id="wb" alt=""style="display:none;" />
+		<img src="http://7xjrp9.com1.z0.glb.clouddn.com/yellow_ball.png" id="yb" alt="" style="display:none;"/>
+		<div class="shotPower">
+			<div id="maxPower">MAX</div>
+			<div id="powbar"><div id="pow"></div></div>
+			<div id="powText">power</div>
+		</div>
+		<div style="position:absolute;left:-100px;top:250px;color:#FFF">进球数：<span id="shotNum">0</span></div>
+	</div>
 
 
 	<div id="content" class="site-content" style="background-size:100%;background:url('http://7xjrp9.com1.z0.glb.clouddn.com/living-room2.jpg')no-repeat">
