ae96a69d80048c71c647c9714a8812a5bd89313c
yizhenkai <1378963360@qq.com>
2016-04-28 20:04:44 +0800

    "主页面跳转报错 The requested URL /homepage/wp-content/themes/zerif-lite/css/images/arrow_right.png was not found on this server."

--- wp-content/themes/zerif-lite/functions.php
+++ wp-content/themes/zerif-lite/functions.php
@@ -346,7 +346,7 @@ function zerif_scripts()
 
 	wp_enqueue_script('snooker_script', get_template_directory_uri() . '/js/soonker.js');
 	
-	wp_enqueue_script('snookerReset_script', get_template_directory_uri() . '/js/livingRoom_snookerReset.js');
+
 
     if (is_singular() && comments_open() && get_option('thread_comments')) {
 
